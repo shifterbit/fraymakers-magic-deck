7548f3d06c773376e8a31641fa1eb8d473a258c2
shifterbit <48891590+shifterbit@users.noreply.github.com>
2024-09-03 22:44:46 -0400

    start with template and add some basic boilerplate code for later use

--- library/scripts/assist/Script.hx
+++ library/scripts/assist/Script.hx
@@ -7,16 +7,101 @@ STATE_FALL = 2;
 STATE_SLAM = 3;
 STATE_OUTRO = 4;
 
+
+
 var SPAWN_X_DISTANCE = 0; // How far in front of player to spawn
 var SPAWN_HEIGHT = 0; // How high up from player to spawn
+var deck = {};
+var deckFull = false;
+
+
+function createSpell(spellFn, predicateFn) {
+	return [spellFn, predicateFn];
+}
+
+function trySpell(spell, score) {
+	if (spell.predicate(score)) {
+		spell.cast();
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function castFirstAvailaleSpell(deck) {
+	var spellList = deck.spells;
+	var score = deck.score;
+	for (spell in spellList) {
+		casted = trySpell(spell, score);
+		if (casted) {
+			return;
+		};
+
+	}
+}
+
+function addCard(event: GameObjectEvent) {
+	var hitboxStats: HitboxStats = event.data.hitboxStats;
+	var damage = hitboxStats.damage;
+	var card = damage % 10;
+	Engine.log(damage);
+	Engine.log(hitboxStats);
+	Engine.log(card);
+	if (deck.cards.length < 3) {
+		deck.cards.push(card);
+	};
+	Engine.log(deck);
+	Engine.log(deck.cards);
+	Engine.log("GOT EVENT");
+
+}
+
+
+function initializeDeckWithSpells(deck, max, spells: Array<Array<Dynamic>>) {
+	var spellset = [];
+	for (spell in spells) {
+		action = spell[0];
+		predicate = spell[1];
+		magicSpell = {
+			cast: actiion,
+			predicate: predicate
+		};
+		spellset.push(magicSpell);
+
+
+	}
+	deck.spells = spellset;
+	deck.max = max;
+	deck.cards = [];
+	deck.score = -1;
+}
+
+function getDeckScore() {
+	if (deck.score != -1) {
+		return deck.score;
+	}
+
+
+	sum = 0;
+	for (card in deck.cards) {
+		sum += card;
+	}
+	deck.score = sum;
+	return deck.score;
+}
+
 
 // Runs on object init
-function initialize(){
+function initialize() {
+	initializeDeckWithSpells(deck, 0, []);
+	Engine.log("Initializing");
+	self.getOwner().addEventListener(GameObjectEvent.HIT_DEALT, addCard, {persistent: true});
+	Engine.log("Added Event Listener");
 	// Face the same direction as the user
 	if (self.getOwner().isFacingLeft()) {
 		self.faceLeft();
 	}
-	
+
 	// Reposition relative to the user
 	Common.repositionToEntityEcb(self.getOwner(), self.flipX(SPAWN_X_DISTANCE), -SPAWN_HEIGHT);
 
@@ -24,7 +109,8 @@ function initialize(){
 	Common.startFadeIn();
 }
 
-function update(){
+
+function update() {
 	// Behavior for each state
 	if (self.inState(STATE_IDLE)) {
 		if (self.finalFramePlayed()) {
@@ -32,13 +118,13 @@ function update(){
 			self.unattachFromFloor();
 			self.setYVelocity(-20);
 			self.updateGameObjectStats({ gravity: 1 });
-			self.toState(STATE_JUMP); 
+			self.toState(STATE_JUMP);
 		}
 	} else if (self.inState(STATE_JUMP)) {
 		// Wait until assist starts to fall
 		if (self.getYVelocity() >= 0) {
 			// Move to fall state
-			self.toState(STATE_FALL); 
+			self.toState(STATE_FALL);
 		}
 	} else if (self.inState(STATE_FALL)) {
 		// Wait until assist lands
@@ -47,20 +133,24 @@ function update(){
 			var proj1 = match.createProjectile(self.getResource().getContent("assisttemplateProjectile"), self);
 			var proj2 = match.createProjectile(self.getResource().getContent("assisttemplateProjectile"), self);
 			proj2.flip(); // Flip the other projectile the opposite way
-			self.toState(STATE_SLAM); 
+			self.toState(STATE_SLAM);
 		}
 	} else if (self.inState(STATE_SLAM)) {
 		if (self.finalFramePlayed()) {
 			// Move to outro state and start fading away
-			self.toState(STATE_OUTRO); 
+			self.toState(STATE_OUTRO);
 			Common.startFadeOut();
 		}
 	} else if (self.inState(STATE_OUTRO)) {
 		if (Common.fadeOutComplete() && self.finalFramePlayed()) {
 			// Destroy
+			self.getOwner().removeEventListener(GameObjectEvent.HIT_DEALT, addCard);
 			self.destroy();
 		}
 	}
 }
-function onTeardown(){
+function onTeardown() {
+
 }
+
+
